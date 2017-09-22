03108ba39c335620a2ae0e5282263f22abf3f7f3
Andrey Turko <andrturko@gmail.com>
2017-09-22 16:10:20 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(strNum1, strNum2) {
+
+    let a1 = strNum1.split('').reverse();
+    let a2 = strNum2.split('').reverse();
+    let res = [];
+    for (let i = 0; i < a1.length; i++) {
+        for (let j = 0; j < a2.length; j++) {
+            let pos = i + j;
+            if (pos >= res.length) {
+                res[pos] = a1[i] * a2[j];
+            }
+            else {
+                res[pos] = a1[i] * a2[j] + res[pos];
+            }
+
+            if (res[pos] > 9) {
+                if (pos + 1 >= res.length) {
+                    res[pos + 1] = Math.floor(res[pos] / 10);
+                }
+                else {
+                    res[pos + 1] = Math.floor(res[pos] / 10) + res[pos + 1];
+                }
+                res[pos] -= Math.floor(res[pos] / 10) * 10;
+            }
+        }
+    }
+    return res.reverse().join("");
 }
